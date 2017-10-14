6c128ab7415c03cdce7019a0078e296bbefc608e
taeiim <ti535666@gmail.com>
2017-10-15 03:56:49 +0900

    save bitmap in gallery

--- Cameraforblind/app/src/main/java/com/example/parktaeim/cameraforblind/Activity/CameraTensorflowActivity.java
+++ Cameraforblind/app/src/main/java/com/example/parktaeim/cameraforblind/Activity/CameraTensorflowActivity.java
@@ -1,8 +1,20 @@
 package com.example.parktaeim.cameraforblind.Activity;
 
+import android.Manifest;
+import android.content.ContentValues;
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.media.ExifInterface;
+import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
+import android.support.annotation.RequiresApi;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.text.method.ScrollingMovementMethod;
 import android.util.Log;
@@ -14,15 +26,18 @@ import android.widget.TextView;
 import com.example.parktaeim.cameraforblind.Classifier;
 import com.example.parktaeim.cameraforblind.R;
 import com.example.parktaeim.cameraforblind.TensorFlowImageClassifier;
-import com.flurgle.camerakit.CameraListener;
-import com.flurgle.camerakit.CameraView;
+import com.google.android.cameraview.CameraView;
 
+import java.io.File;
+import java.io.FileOutputStream;
+import java.util.Calendar;
 import java.util.List;
 import java.util.Locale;
 import java.util.concurrent.Executor;
 import java.util.concurrent.Executors;
 
 import android.speech.tts.TextToSpeech;
+import android.widget.Toast;
 
 
 /**
@@ -40,6 +55,7 @@ public class CameraTensorflowActivity extends AppCompatActivity {
     private static final String MODEL_FILE = "file:///android_asset/tensorflow_inception_graph.pb";
     private static final String LABEL_FILE =
             "file:///android_asset/imagenet_comp_graph_label_strings.txt";
+    private static final int MY_PERMISSIONS_REQUEST_READ_EXTERNAL_STORAGE = 100;
 
     private Classifier classifier;
     private Executor executor = Executors.newSingleThreadExecutor();
@@ -62,64 +78,215 @@ public class CameraTensorflowActivity extends AppCompatActivity {
         btnToggleCamera = (Button) findViewById(R.id.btnToggleCamera);
         btnDetectObject = (Button) findViewById(R.id.btnDetectObject);
 
-        cameraView.setCameraListener(new CameraListener() {
-            @Override
-            public void onPictureTaken(byte[] picture) {
-                super.onPictureTaken(picture);
+        if (cameraView != null) {
+            cameraView.addCallback(mCallback);
+        }
+//        cameraView.takePicture();
 
-                Bitmap bitmap = BitmapFactory.decodeByteArray(picture, 0, picture.length);
-
-                bitmap = Bitmap.createScaledBitmap(bitmap, INPUT_SIZE, INPUT_SIZE, false);
-
-                imageViewResult.setImageBitmap(bitmap);
-
-                final List<Classifier.Recognition> results = classifier.recognizeImage(bitmap);
-
-                Log.d("result message",results.toString());
-
-                resultString = results.toString();
-                String pattern =  "^[a-zA-Z]*$";
-
-                resultString = resultString.replaceAll("[^a-zA-Z,]","");
-                Log.d("resultString",resultString);
-
-                myTTS = new TextToSpeech(getApplicationContext(),new TextToSpeech.OnInitListener(){
-                    @Override
-                    public void onInit(int i) {
-                        myTTS.setLanguage(Locale.KOREAN);
-                        myTTS.speak(resultString,TextToSpeech.QUEUE_FLUSH,null);
-                    }
-                });
-
-//                Pattern p = Pattern.compile("(^[a-zA-Z]*$)");
+//        cameraView.setCameraListener(new CameraListener() {
+//            @Override
+//            public void onPictureTaken(byte[] picture) {
+//                super.onPictureTaken(picture);
 //
-//                String regExp = "";
-//                String englishResult = resultString.replaceAll("^[a-zA-Z]*$", "");
-//                Log.d("englishResult",p.toString());
-//                resultString = pattern;
-//                Pattern pattern1 = Pattern.compile(regExp);
-//                Matcher matcher = Pattern.ma
-
-                textViewResult.setText(results.toString());
-            }
-        });
-
+//                Bitmap bitmap = BitmapFactory.decodeByteArray(picture, 0, picture.length);
+//
+//                bitmap = Bitmap.createScaledBitmap(bitmap, INPUT_SIZE, INPUT_SIZE, false);
+//
+//                imageViewResult.setImageBitmap(bitmap);
+//
+//                String title = "image11";
+//
+//                Context context = getApplicationContext();
+//                String filePath = (context.getExternalCacheDir()).toString() + "/" + title + ".jpg";
+//
+////                addImageToGallery(filePath,context);
+//                imageSave(bitmap);
+//                final List<Classifier.Recognition> results = classifier.recognizeImage(bitmap);
+//
+//                Log.d("result message",results.toString());
+//
+//                resultString = results.toString();
+//                String pattern =  "^[a-zA-Z]*$";
+//
+//                resultString = resultString.replaceAll("[^a-zA-Z,]","");
+//                Log.d("resultString",resultString);
+//
+//                myTTS = new TextToSpeech(getApplicationContext(),new TextToSpeech.OnInitListener(){
+//                    @Override
+//                    public void onInit(int i) {
+//                        myTTS.setLanguage(Locale.KOREAN);
+//                        myTTS.speak(resultString,TextToSpeech.QUEUE_FLUSH,null);
+//                    }
+//                });
+//
+//                textViewResult.setText(results.toString());
+//            }
+//        });
+//
         btnToggleCamera.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                cameraView.toggleFacing();
+                if (cameraView != null) {
+                    int facing = cameraView.getFacing();
+                    cameraView.setFacing(facing == CameraView.FACING_FRONT ?
+                            CameraView.FACING_BACK : CameraView.FACING_FRONT);
+                }
             }
         });
 
         btnDetectObject.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                cameraView.captureImage();
+                cameraView.takePicture();
             }
         });
 
         initTensorFlowAndLoadModel();
     }
+
+//    public static void addImageToGallery(final String filePath, final Context context) {
+//
+//        ContentValues values = new ContentValues();
+//
+//        values.put(MediaStore.Images.Media.DATE_TAKEN, System.currentTimeMillis());
+//        values.put(MediaStore.Images.Media.MIME_TYPE, "image/jpeg");
+//        values.put(MediaStore.MediaColumns.DATA, filePath);
+//
+//        context.getContentResolver().insert(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, values);
+//
+//        Toast.makeText(context,"파일 저장",Toast.LENGTH_SHORT).show();
+//    }
+
+    private void imageSave(Bitmap finalBitmap) {
+        Context context = getApplicationContext();
+        Calendar calendar = Calendar.getInstance();
+        String title = "image11";
+        String filePath = (context.getExternalCacheDir()).toString() + "/" + title + ".jpg";
+
+
+
+        try{
+            File file = new File(title);
+            FileOutputStream fos = openFileOutput(title,0);
+            finalBitmap.compress(Bitmap.CompressFormat.JPEG,100,fos);
+            fos.flush();
+            fos.close();
+
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+
+        Intent mediaScanIntent = new Intent(Intent.ACTION_MEDIA_SCANNER_SCAN_FILE);
+        File f = new File(filePath);
+        Uri contentUri = Uri.fromFile(f);
+        mediaScanIntent.setData(contentUri);
+        this.sendBroadcast(mediaScanIntent);
+
+        checkPermission();
+        MediaStore.Images.Media.insertImage(getContentResolver(), finalBitmap, "title", "descripton");
+
+        Toast.makeText(this,"파일 저장 완료",Toast.LENGTH_SHORT).show();
+
+//        String root = Environment.getExternalStorageDirectory().toString();
+//        File myDir = new File(root);
+//        myDir.mkdirs();
+//        String fname = "Image-" + image_name+ ".jpg";
+//        ContentValues values = new ContentValues();
+//        values.put(MediaStore.Images.Media.DESCRIPTION,description);
+//        values.put(MediaStore.Images.Media.DESCRIPTION,description);
+//        File file = new File(myDir, fname);
+//        if (file.exists()) file.delete();
+//        Log.i("LOAD", root + fname);
+//        try {
+//            FileOutputStream out = new FileOutputStream(file);
+//            finalBitmap.compress(Bitmap.CompressFormat.JPEG, 90, out);
+//            out.flush();
+//            out.close();
+//        } catch (Exception e) {
+//            e.printStackTrace();
+//        }
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.M)
+    private void checkPermission() {
+        if (ContextCompat.checkSelfPermission(this,Manifest.permission.READ_EXTERNAL_STORAGE)
+                != PackageManager.PERMISSION_GRANTED) {
+
+            // Should we show an explanation?
+            if (shouldShowRequestPermissionRationale(
+                    Manifest.permission.READ_EXTERNAL_STORAGE)) {
+                // Explain to the user why we need to read the contacts
+            }
+
+            requestPermissions(new String[]{Manifest.permission.READ_EXTERNAL_STORAGE},
+                    MY_PERMISSIONS_REQUEST_READ_EXTERNAL_STORAGE);
+
+            // MY_PERMISSIONS_REQUEST_READ_EXTERNAL_STORAGE is an
+            // app-defined int constant that should be quite unique
+
+            return;
+        }
+    }
+
+    private CameraView.Callback mCallback = new CameraView.Callback() {
+        @Override
+        public void onCameraOpened(CameraView cameraView) {
+            super.onCameraOpened(cameraView);
+        }
+
+        @Override
+        public void onCameraClosed(CameraView cameraView) {
+            super.onCameraClosed(cameraView);
+        }
+
+        @Override
+        public void onPictureTaken(CameraView cameraView, byte[] data) {
+            super.onPictureTaken(cameraView, data);
+
+            Bitmap bitmap = BitmapFactory.decodeByteArray(data, 0, data.length);
+
+            bitmap = Bitmap.createScaledBitmap(bitmap, INPUT_SIZE, INPUT_SIZE, false);
+
+            ExifInterface exif = new ExifInterface(imagePath);
+            int exifOrientation = exif.getAttributeInt(
+                    ExifInterface.TAG_ORIENTATION, ExifInterface.ORIENTATION_NORMAL);
+            int exifDegree = exifOrientationToDegrees(exifOrientation);
+            image = rotate(image, exifDegree);
+
+            imageViewResult.setImageBitmap(bitmap);
+
+            String title = "image11";
+
+            Context context = getApplicationContext();
+            String filePath = (context.getExternalCacheDir()).toString() + "/" + title + ".jpg";
+
+//                addImageToGallery(filePath,context);
+            imageSave(bitmap);
+            final List<Classifier.Recognition> results = classifier.recognizeImage(bitmap);
+
+            Log.d("result message",results.toString());
+
+            resultString = results.toString();
+            String pattern =  "^[a-zA-Z]*$";
+
+            resultString = resultString.replaceAll("[^a-zA-Z,]","");
+            Log.d("resultString",resultString);
+
+            myTTS = new TextToSpeech(getApplicationContext(),new TextToSpeech.OnInitListener(){
+                @Override
+                public void onInit(int i) {
+                    myTTS.setLanguage(Locale.KOREAN);
+                    myTTS.speak(resultString,TextToSpeech.QUEUE_FLUSH,null);
+                }
+            });
+
+            textViewResult.setText(results.toString());
+
+        }
+
+//
+    };
+
 
     @Override
     protected void onResume() {
