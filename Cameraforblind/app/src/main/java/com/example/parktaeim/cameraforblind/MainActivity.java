0cf318257768fb8bece19af24c931258380bc384
Sumin Go <jennifer045020@gmail.com>
2017-10-14 13:14:11 +0900

    Create Camera

--- Cameraforblind/app/src/main/java/com/example/parktaeim/cameraforblind/MainActivity.java
+++ Cameraforblind/app/src/main/java/com/example/parktaeim/cameraforblind/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.parktaeim.cameraforblind;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.WindowManager;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +10,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
     }
 }
