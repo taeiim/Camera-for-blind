09a288a2e40ef6954accdfc3eb5a0faf962c939c
Sumin Go <jennifer045020@gmail.com>
2017-10-14 16:27:57 +0900

    Create Camera

--- Cameraforblind/app/src/main/java/com/example/parktaeim/cameraforblind/Activity/MainActivity.java
+++ Cameraforblind/app/src/main/java/com/example/parktaeim/cameraforblind/Activity/MainActivity.java
@@ -1,12 +1,26 @@
 package com.example.parktaeim.cameraforblind.Activity;
 
+import android.Manifest;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.TextureView;
 import android.view.WindowManager;
+import android.widget.Toast;
 
+import com.example.parktaeim.cameraforblind.Preview;
 import com.example.parktaeim.cameraforblind.R;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TextureView cameraView;
+    private Preview preview;
+
+    private static final String TAG = "MAINACTIVITY";
+    static final int REQUEST_CAMERA = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -14,6 +28,42 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        cameraView = (TextureView) findViewById(R.id.cameraTextureView);
+        preview = new Preview(this, cameraView);
+    }
 
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        switch (requestCode) {
+            case REQUEST_CAMERA:
+                for (int i = 0; i < permissions.length; i++) {
+                    String permission = permissions[i];
+                    int grantResult = grantResults[i];
+                    if (permission.equals(Manifest.permission.CAMERA)) {
+                        if (grantResult == PackageManager.PERMISSION_GRANTED) {
+                            cameraView = (TextureView) findViewById(R.id.cameraTextureView);
+                            preview = new Preview(this, cameraView);
+                            Log.d(TAG, "mPreview set");
+                        } else {
+                            Toast.makeText(this, "Should have camera permission to run", Toast.LENGTH_LONG).show();
+                            finish();
+                        }
+                    }
+                }
+                break;
+        }
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        preview.onResume();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        preview.onPause();
     }
 }
--- Cameraforblind/app/src/main/java/com/example/parktaeim/cameraforblind/Preview.java
+++ Cameraforblind/app/src/main/java/com/example/parktaeim/cameraforblind/Preview.java
@@ -1,37 +1,198 @@
 package com.example.parktaeim.cameraforblind;
 
+import android.Manifest;
+import android.annotation.TargetApi;
+import android.app.Activity;
 import android.content.Context;
+import android.content.pm.PackageManager;
+import android.graphics.SurfaceTexture;
+import android.hardware.camera2.CameraAccessException;
+import android.hardware.camera2.CameraCaptureSession;
+import android.hardware.camera2.CameraCharacteristics;
+import android.hardware.camera2.CameraDevice;
+import android.hardware.camera2.CameraManager;
+import android.hardware.camera2.CameraMetadata;
+import android.hardware.camera2.CaptureRequest;
+import android.hardware.camera2.params.StreamConfigurationMap;
+import android.os.Build;
+import android.os.Handler;
+import android.os.HandlerThread;
+import android.support.annotation.IntDef;
+import android.support.annotation.NonNull;
+import android.support.annotation.RequiresApi;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.util.Size;
+import android.view.Surface;
 import android.view.SurfaceHolder;
 import android.view.SurfaceView;
+import android.view.TextureView;
 import android.view.ViewGroup;
+import android.widget.Toast;
+
+import com.example.parktaeim.cameraforblind.Activity.MainActivity;
+
+import java.util.Arrays;
+import java.util.concurrent.Semaphore;
 
 /**
  * Created by user on 2017-10-14.
  */
 
-public class Preview extends ViewGroup implements SurfaceHolder.Callback {
+@RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
+public class Preview extends Thread{
 
-    Preview(Context context, SurfaceView surfaceView) {
-        super(context);
+    private final static String TAg = "Preview : ";
+
+    private Size previewSize;
+    private Context context;
+    private CameraDevice cameraDevice;
+    private CaptureRequest.Builder previewBuilder;
+    private CameraCaptureSession previewSession;
+    private TextureView textureView;
+
+    public Preview(Context context, TextureView textureView) {
+        this.context = context;
+        this.textureView = textureView;
     }
 
-    @Override
-    public void surfaceCreated(SurfaceHolder surfaceHolder) {
-
+    @TargetApi(Build.VERSION_CODES.LOLLIPOP)
+    private String getBackFacingCameraId(CameraManager manager) {
+        try {
+            for(final String cameraId : manager.getCameraIdList()) {
+                CameraCharacteristics charactaristics = manager.getCameraCharacteristics(cameraId);
+                int orientation = charactaristics.get(CameraCharacteristics.LENS_FACING);
+                if(orientation == CameraCharacteristics.LENS_FACING_BACK) {
+                    return cameraId;
+                }
+            }
+        } catch (CameraAccessException e) {}
+        return null;
     }
 
-    @Override
-    public void surfaceChanged(SurfaceHolder surfaceHolder, int i, int i1, int i2) {
+    @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
+    public void openCamera() {
+        CameraManager manager = (CameraManager) context.getSystemService(Context.CAMERA_SERVICE);
+        try {
+            String cameraId = getBackFacingCameraId(manager);
+            CameraCharacteristics characteristic = manager.getCameraCharacteristics(cameraId);
+            StreamConfigurationMap map = characteristic.get(CameraCharacteristics.SCALER_STREAM_CONFIGURATION_MAP);
+            previewSize = map.getOutputSizes(SurfaceTexture.class)[0];
 
+            int permissinoCamera = ContextCompat.checkSelfPermission(context, Manifest.permission.CAMERA);
+            if(permissinoCamera == PackageManager.PERMISSION_DENIED) {
+                ActivityCompat.requestPermissions((Activity) context, new String[]{Manifest.permission.CAMERA}, /*MainActivity.REQUEST_CAMERA*/1);
+            }
+        }catch (CameraAccessException e) {}
     }
 
-    @Override
-    public void surfaceDestroyed(SurfaceHolder surfaceHolder) {
+    private TextureView.SurfaceTextureListener textureListener = new TextureView.SurfaceTextureListener() {
 
+        @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
+        @Override
+        public void onSurfaceTextureAvailable(SurfaceTexture surfaceTexture, int width, int height) {
+            openCamera();
+        }
+
+        @Override
+        public void onSurfaceTextureSizeChanged(SurfaceTexture surfaceTexture, int width, int height) {
+
+        }
+
+        @Override
+        public boolean onSurfaceTextureDestroyed(SurfaceTexture surfaceTexture) {
+            return false;
+        }
+
+        @Override
+        public void onSurfaceTextureUpdated(SurfaceTexture surfaceTexture) {
+
+        }
+    };
+
+    private CameraDevice.StateCallback stateCallback = new CameraDevice.StateCallback() {
+        @Override
+        public void onOpened(@NonNull CameraDevice camera) {
+            cameraDevice = camera;
+            startPreview();
+        }
+
+        @Override
+        public void onDisconnected(@NonNull CameraDevice cameraDevice) {
+
+        }
+
+        @Override
+        public void onError(@NonNull CameraDevice cameraDevice, int i) {
+
+        }
+    };
+
+    protected void startPreview() {
+        /*if(null = cameraDevice || textureView.isAvailable() || null == previewSize) {
+
+        }*/
+
+        SurfaceTexture surfaceTexture = textureView.getSurfaceTexture();
+        if(null == surfaceTexture) {return;}
+
+        surfaceTexture.setDefaultBufferSize(previewSize.getWidth(), previewSize.getHeight());
+        Surface surface = new Surface(surfaceTexture);
+
+        try {
+            previewBuilder = cameraDevice.createCaptureRequest(CameraDevice.TEMPLATE_PREVIEW);
+        } catch (CameraAccessException e) {}
+
+        previewBuilder.addTarget(surface);
+
+        try {
+            cameraDevice.createCaptureSession(Arrays.asList(surface), new CameraCaptureSession.StateCallback() {
+                @Override
+                public void onConfigured(@NonNull CameraCaptureSession session) {
+                    previewSession = session;
+                    updatePreview();
+                }
+
+                @Override
+                public void onConfigureFailed(@NonNull CameraCaptureSession cameraCaptureSession) {
+                    Toast.makeText(context, "onConfigureFailed", Toast.LENGTH_SHORT).show();
+                }
+            }, null);
+        } catch (CameraAccessException e) {}
     }
 
-    @Override
-    protected void onLayout(boolean b, int i, int i1, int i2, int i3) {
+    protected void updatePreview() {
+        previewBuilder.set(CaptureRequest.CONTROL_MODE, CameraMetadata.CONTROL_MODE_AUTO);
+        HandlerThread thread = new HandlerThread("CameraPreview");
+        thread.start();
+        Handler backgroundhandler = new Handler(thread.getLooper());
 
+        try {
+            previewSession.setRepeatingRequest(previewBuilder.build(), null, backgroundhandler);
+        }catch (CameraAccessException e) {}
+    }
+
+    public void setSurfaceTextureListener() {
+        textureView.setSurfaceTextureListener(textureListener);
+    }
+
+    public void onResume() {
+        setSurfaceTextureListener();
+    }
+
+    private Semaphore cameraOpenCloseLock = new Semaphore(1);
+
+    public void onPause() {
+        try {
+            cameraOpenCloseLock.acquire();
+            if (null != cameraDevice) {
+                cameraDevice.close();
+                cameraDevice = null;
+            }
+        } catch (InterruptedException e) {
+            throw new RuntimeException("Interrupted while trying to lock camera closing");
+        } finally {
+            cameraOpenCloseLock.release();
+        }
     }
 }
