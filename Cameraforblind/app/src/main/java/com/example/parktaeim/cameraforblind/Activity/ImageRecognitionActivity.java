c7362684d2753b8fa2140c569f3b86dcde52d9b1
Sumin Go <jennifer045020@gmail.com>
2017-10-15 13:00:19 +0900

    FaceDectator

--- Cameraforblind/app/src/main/java/com/example/parktaeim/cameraforblind/Activity/ImageRecognitionActivity.java
+++ Cameraforblind/app/src/main/java/com/example/parktaeim/cameraforblind/Activity/ImageRecognitionActivity.java
@@ -3,15 +3,26 @@ package com.example.parktaeim.cameraforblind.Activity;
 import android.app.Activity;
 import android.content.Intent;
 import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.RectF;
+import android.graphics.drawable.BitmapDrawable;
 import android.os.Bundle;
 import android.provider.MediaStore;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.util.SparseArray;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.Toast;
 
 import com.example.parktaeim.cameraforblind.R;
+import com.google.android.gms.vision.Frame;
+import com.google.android.gms.vision.face.Face;
+import com.google.android.gms.vision.face.FaceDetector;
 
 /**
  * Created by parktaeim on 2017. 10. 14..
@@ -55,6 +66,43 @@ public class ImageRecognitionActivity extends AppCompatActivity {
                     Bitmap image_bitmap = MediaStore.Images.Media.getBitmap(getContentResolver(),data.getData());
                     selectedImageView.setImageBitmap(image_bitmap);
 
+                    final Bitmap myBitmap = ((BitmapDrawable)selectedImageView.getDrawable()).getBitmap();
+                    selectedImageView.setImageBitmap(myBitmap);
+
+                    final Paint rectPaint = new Paint();
+                    rectPaint.setStrokeWidth(5);
+                    rectPaint.setColor(Color.RED);
+                    rectPaint.setStyle(Paint.Style.STROKE);
+
+                    final Bitmap tempBitmap = Bitmap.createBitmap(myBitmap.getWidth(), myBitmap.getHeight(), Bitmap.Config.RGB_565);
+                    final Canvas canvas = new Canvas(tempBitmap);
+                    canvas.drawBitmap(myBitmap,0,0,null);
+
+                    FaceDetector faceDetector = new FaceDetector.Builder(getApplicationContext())
+                            .setTrackingEnabled(false)
+                            .setClassificationType(FaceDetector.ALL_CLASSIFICATIONS)
+                            .setLandmarkType(FaceDetector.ALL_LANDMARKS)
+                            .setMode(FaceDetector.FAST_MODE)
+                            .build();
+
+                    if(!faceDetector.isOperational()){
+                        Toast.makeText(getApplicationContext(), "no", Toast.LENGTH_SHORT).show();
+                        return;
+                    }
+                    Frame frame = new Frame.Builder().setBitmap(myBitmap).build();
+                    SparseArray<Face> sparseArray = faceDetector.detect(frame);
+
+                    for(int i =  0; i < sparseArray.size(); i++) {
+                        Face face = sparseArray.valueAt(i);
+                        float x1 = face.getPosition().x;
+                        float y1 = face.getPosition().y;
+                        float x2 = x1 + face.getWidth();
+                        float y2 = y1 + face.getWidth();
+                        RectF rectF = new RectF(x1,y1,x2,y2);
+                        canvas.drawRoundRect(rectF,2,2,rectPaint);
+                    }
+
+                    selectedImageView.setImageDrawable(new BitmapDrawable(getResources(), tempBitmap));
                 }catch (Exception e){
                     e.printStackTrace();
                 }
--- Cameraforblind/app/src/main/java/com/example/parktaeim/cameraforblind/Activity/OcrGraphic.java
+++ Cameraforblind/app/src/main/java/com/example/parktaeim/cameraforblind/Activity/OcrGraphic.java
@@ -1,25 +1,5 @@
 package com.example.parktaeim.cameraforblind.Activity;
 
-/**
- * Created by parktaeim on 2017. 10. 15..
- */
-
-/*
- * Copyright (C) The Android Open Source Project
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
         import android.graphics.Canvas;
         import android.graphics.Color;
         import android.graphics.Paint;
@@ -31,10 +11,6 @@ package com.example.parktaeim.cameraforblind.Activity;
 
         import java.util.List;
 
-/**
- * Graphic instance for rendering TextBlock position, size, and ID within an associated graphic
- * overlay view.
- */
 public class OcrGraphic extends GraphicOverlay.Graphic {
 
     private int mId;
